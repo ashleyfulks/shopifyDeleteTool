969db60a966317d53ba4fec98452c59cef086a25
Ashley Fulks <ashley.fulks@shopify.com>
2019-04-28 11:24:05 -0700

    Changes to package.json
    Changes

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const Index = () => (
+<div>
+                        <h1>Shopify Delete Tool</h1>
+                        <p>This tool makes it easy to delete all your products or customers on shopify. The admin unfortunately does not allow deleting of a few hundred products at a time without stopping. This app however will not quit until all your products or customers are purged. Please be very careful with this app! EXPORT YOUR PRODUCTS OR CUSTOMERS BEFORE USING THIS APP JUST IN CASE.</p>
+</div>           
+);
+
+export default Index;
